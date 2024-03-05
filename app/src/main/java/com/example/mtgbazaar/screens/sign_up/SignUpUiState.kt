24a63a95756bfed0a93b748a0f8bf3cd116769e9
jcfenoglio <jcfenoglio@gmail.com>
2024-03-04 17:14:06 -0800

    fix typo in SignUpUiState

--- app/src/main/java/com/example/mtgbazaar/screens/sign_up/SignUpUiState.kt
+++ app/src/main/java/com/example/mtgbazaar/screens/sign_up/SignUpUiState.kt
@@ -3,5 +3,5 @@ package com.example.mtgbazaar.screens.sign_up
 data class SignUpUiState(
     val email: String = "",
     val password: String = "",
-    val repeatPassword: String = "0
+    val repeatPassword: String = ""
 )
