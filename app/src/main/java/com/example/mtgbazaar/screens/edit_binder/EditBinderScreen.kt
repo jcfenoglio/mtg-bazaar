f998a3ae6d5a449df43d4c42ec86d44e9ae935a9
jcfenoglio <jcfenoglio@gmail.com>
2024-03-19 15:26:36 -0700

    Minor color fixes

--- app/src/main/java/com/example/mtgbazaar/screens/edit_binder/EditBinderScreen.kt
+++ app/src/main/java/com/example/mtgbazaar/screens/edit_binder/EditBinderScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.Wallpapers
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.mtgbazaar.common.composable.ActionToolbar
 import com.example.mtgbazaar.common.composable.BasicField
